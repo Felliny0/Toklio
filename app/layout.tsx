2211c05dc3c967a9dc5f4efb8193c1073a4dc5a4
Felliny0 <furkanakyel18@gmail.com>
2026-05-17 21:15:31 +0200

    seo compete

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {export const metadata: Metadata = {
+export const metadata: Metadata = {
   title:
     "Toklio - TikTok Video Downloader Without Watermark | HD TikTok Saver",
 
