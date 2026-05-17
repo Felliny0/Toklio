86114dc6db006267eabb462380ea82f87900f157
Felliny0 <furkanakyel18@gmail.com>
2026-05-17 20:39:50 +0200

    fix contact

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+export default function ContactPage() {
+  return (
+    <main className="min-h-screen bg-black text-white p-10">
+      <h1 className="text-5xl font-black mb-6">
+        Contact
+      </h1>
+
+      <p className="text-zinc-400 text-lg">
+        Contact us: support@toklio.app
+      </p>
+    </main>
+  )
+}
