e15ba2a0343c2f672852149dbb14034e16636f5b
Felliny0 <furkanakyel18@gmail.com>
2026-05-17 20:46:27 +0200

    fix pr'vacy

---
+++ app/privacy/page.tsx
@@ -0,0 +1,13 @@
+export default function PrivacyPage() {
+  return (
+    <main className="min-h-screen bg-black text-white p-10">
+      <h1 className="text-5xl font-black mb-6">
+        Privacy Policy
+      </h1>
+
+      <p className="text-zinc-400 text-lg">
+        Toklio does not store downloaded videos or user data.
+      </p>
+    </main>
+  )
+}
