648ebf5712ba9ec0787dbef96f6044d6dd8c4459
Felliny0 <furkanakyel18@gmail.com>
2026-05-18 18:27:50 +0200

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -114,7 +114,18 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    > 
+    > <script async src="https://www.googletagmanager.com/gtag/js?id=G-NBV4K3RP20"></script>
+
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-NBV4K3RP20');
+    `,
+  }}
+/>
   
   
     <script
